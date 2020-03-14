6432ab0968a19e2cb78f69d272eeffbcdd3fb285
Automaton <automatonatm@gmail.com>
2020-03-14 07:23:11 +0100

    save state

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React,  {Component} from "react"
+
+import Aux from '../../hoc/Ax'
+
+class BurgerBuilder extends Component{
+    render() {
+        return(
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder
